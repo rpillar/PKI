54f111c4aa48f37d3c175c7f510a0a0b88897a6c
Gogs <richardpillar@googlemail.com>
2019-12-28 17:25:23 +0000

    more minor updates - still trying to get inheritance parsing working.

--- script/perl_files.pl
+++ script/perl_files.pl
@@ -243,11 +243,13 @@ sub _parse_inheritance {
         }
     }
 
-    if ($line =~ m/BEGIN\s+\{\s+extends\s+\'([\w\:]+)\s*\}*/ ) {
+    if ($line =~ m/BEGIN\s+\{*\s*extends\s+'*([\w\:]+)'*\s*\}*/ ) {
+        p $line;
         ( my $list = $1 ) =~ s/\s+\#.*//;
         $list =~ s/[\r\n]//;
         my (@mods) = Safe->new()->reval($list);
         foreach my $mod (@mods) {
+            p $mod;
             $file_data = _util_dpush($file_data, 'parent', $mod);
         }
     }
