27337c32e1238dc8228228f536120d4d5fcedade
Gogs <richardpillar@googlemail.com>
2020-02-20 08:28:41 +0000

    Update initial.sql to match schema

--- initial.sql
+++ initial.sql
@@ -1,5 +1,5 @@
 CREATE TABLE metrics(module text, subname text, complexity integer, lines integer);
-CREATE TABLE summary(module text, max_complexity integer, lines integer, pod integer);
+CREATE TABLE summary(module text, max_complexity integer, lines integer, pod integer, avg_complexity integer, sub_count integer);
 CREATE TABLE critic(id integer primary key, module text, critic text, line_number integer);
 CREATE TABLE dependencies(id integer primary key, module text, dependencies text);
 CREATE TABLE inheritance(id integer primary key, module text, inheritance text);
