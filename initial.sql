26e802605b1fb9d672a2bbd5fbabf944fefc2368
Gogs <richardpillar@googlemail.com>
2020-07-24 20:04:43 +0100

    Updates - update sql schema.

--- initial.sql
+++ initial.sql
@@ -5,10 +5,52 @@ DROP TABLE IF EXISTS dependencies;
 DROP TABLE IF EXISTS inheritance;
 DROP TABLE IF EXISTS gitlog;
 DROP TABLE IF EXISTS gitcommits;
-CREATE TABLE metrics(module text, subname text, complexity integer, lines integer);
-CREATE TABLE summary(module text, max_complexity integer, lines integer, pod integer, avg_complexity integer, sub_count integer);
-CREATE TABLE critic(id integer primary key, module text, critic text, line_number integer, source text, explanation text);
-CREATE TABLE dependencies(id integer primary key, module text, dependencies text);
-CREATE TABLE inheritance(id integer primary key, module text, inheritance text);
-CREATE TABLE gitlog(module text, log text);
-CREATE TABLE gitcommits(date text, commits integer);
+CREATE TABLE metrics(
+    id integer primary key autoincrement,
+    module text, 
+    subname text, 
+    complexity integer, 
+    lines integer
+);
+CREATE INDEX idx_metrics_module ON metrics (module);
+CREATE TABLE summary(
+    id integer primary key autoincrement,
+    module text, 
+    max_complexity integer, 
+    lines integer, 
+    pod integer, 
+    avg_complexity integer, 
+    sub_count integer
+);
+CREATE INDEX idx_summary_module ON summary (module);
+CREATE TABLE critic(
+    id integer primary key autoincrement, 
+    module text, 
+    critic text, 
+    line_number integer, 
+    source text, 
+    explanation text
+);
+CREATE INDEX idx_critic_module ON critic (module);
+CREATE TABLE dependencies(
+    id integer primary key autoincrement, 
+    module text, 
+    dependencies text
+);
+CREATE INDEX idx_dependencies_module ON dependencies (module);
+CREATE TABLE inheritance(
+    id integer primary key autoincrement, 
+    module text, 
+    inheritance text
+);
+CREATE INDEX idx_inheritance_module ON inheritance (module);
+CREATE TABLE gitlog(
+    module text, 
+    log text
+);
+CREATE INDEX idx_gitlog_module ON gitlog (module);
+CREATE TABLE gitcommits(
+    date text, 
+    commits integer
+);
+CREATE INDEX idx_gitcommits_module ON gitcommits (date);
